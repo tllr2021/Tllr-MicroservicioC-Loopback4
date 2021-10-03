be46ff15638040d7877b55a93123fd6c35f4dc80
Raúl <raulguzmanguerrero@outlook.com>
2021-10-03 14:40:32 -0500

    Feat Envios: Add DatosEnvio model

---
+++ src/models/datos-envio.model.ts
@@ -0,0 +1,40 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model()
+export class DatosEnvio extends Model {
+  @property({
+    type: 'array',
+    itemType: 'string',
+    required: true,
+  })
+  metodosPago: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  subtotal: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  peso: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  codigoPostal: string;
+
+
+  constructor(data?: Partial<DatosEnvio>) {
+    super(data);
+  }
+}
+
+export interface DatosEnvioRelations {
+  // describe navigational properties here
+}
+
+export type DatosEnvioWithRelations = DatosEnvio & DatosEnvioRelations;
