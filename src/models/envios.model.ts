f80f9eaf82eec706960fea7592616ba23b81de3f
Raúl <raulguzmanguerrero@outlook.com>
2021-10-03 14:41:14 -0500

    Feat Envios: Add Envios model

---
+++ src/models/envios.model.ts
@@ -0,0 +1,32 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Envios extends Model {
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precioEnvio: number;
+
+  @property({
+    type: 'object',
+    required: true,
+  })
+  descuentos: object;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tiempoEnvio: String;
+
+  constructor(data?: Partial<Envios>) {
+    super(data);
+  }
+}
+
+export interface EnviosRelations {
+  // describe navigational properties here
+}
+
+export type EnviosWithRelations = Envios & EnviosRelations;
